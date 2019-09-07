6751cb73eeaaa54862ef1329cccc1b86b426f919
Mark Shevelev <lionishy@gmail.com>
2019-09-07 19:45:57 +0300

    SimpleTableIO: ascii stream input added

--- SimpleTableIO.h
+++ SimpleTableIO.h
@@ -5,6 +5,7 @@
 #include "SimpleTable.h"
 
 #include <iostream>
+#include <limits>
 
 template <typename T, size_t Dim>
 std::ostream &operator<<(std::ostream &ascii_os, iki::Argument<T, Dim> const &argument) {
@@ -29,6 +30,24 @@ std::ostream& operator<<(std::ostream &ascii_os, iki::UniformSimpleTable<T, Dim,
 	}
 
 	return ascii_os << flush;
+}
+
+template <typename T, size_t Dim>
+std::istream &operator>>(std::istream &ascii_is, iki::UniformSpace<T, Dim> &space) {
+	for (size_t axis_idx = 0u; axis_idx != Dim; ++axis_idx)
+		ascii_is >> space.axes[axis_idx].begin >> space.axes[axis_idx].step;
+	return ascii_is;
+}
+
+template <typename T, size_t Dim, size_t Scale>
+std::istream &operator>>(std::istream &ascii_is, iki::UniformSimpleTable<T, Dim, Scale> &table) {
+	for (size_t collapsed_idx = 0u, end = iki::collapsed_size(table.bounds); collapsed_idx != end; ++collapsed_idx) {
+		for (size_t arg_idx = 0u; arg_idx != Dim; ++arg_idx)
+			ascii_is.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
+		for (size_t scl_idx = 0u; scl_idx != Scale; ++scl_idx)
+			ascii_is >> table.data[scl_idx+collapsed_idx*Scale];
+	}
+	return ascii_is;
 }
 
 #endif /* SimpleTableIO_H */
