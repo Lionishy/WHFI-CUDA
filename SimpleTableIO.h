58f2e62bd2171bba3c386345a82abca6efdef2ad
Mark Shevelev <lionishy@gmail.com>
2019-09-07 20:06:49 +0300

    SimpleTableIO fix: collapsed_size expects pointer

--- SimpleTableIO.h
+++ SimpleTableIO.h
@@ -41,7 +41,7 @@ std::istream &operator>>(std::istream &ascii_is, iki::UniformSpace<T, Dim> &spac
 
 template <typename T, size_t Dim, size_t Scale>
 std::istream &operator>>(std::istream &ascii_is, iki::UniformSimpleTable<T, Dim, Scale> &table) {
-	for (size_t collapsed_idx = 0u, end = iki::collapsed_size(table.bounds); collapsed_idx != end; ++collapsed_idx) {
+	for (size_t collapsed_idx = 0u, end = iki::collapsed_size(&table.bounds); collapsed_idx != end; ++collapsed_idx) {
 		for (size_t arg_idx = 0u; arg_idx != Dim; ++arg_idx)
 			ascii_is.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
 		for (size_t scl_idx = 0u; scl_idx != Scale; ++scl_idx)
