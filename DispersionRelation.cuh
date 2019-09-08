4262c7ac1573314b6cfca381e09f72aab5ed9295
Mark Shevelev <lionishy@gmail.com>
2019-09-08 22:44:22 +0300

    DispersionRelation and DispersionRelationDerivative added -- device based

--- DispersionRelation.cuh
+++ DispersionRelation.cuh
@@ -7,10 +7,38 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-namespace iki {	namespace whfi { namespace cuda {
-	template <typename T>
-	__device__ T dispersion_realation(PhysicalParamenters<T> params, T omega, T k) {
-	}
+namespace iki {	namespace whfi { namespace device {
+	template <typename T, typename F_t>
+	struct DispersionRelation {
+		__device__ T operator()(T omega, T k) {
+			return T(1. / 1836.) + k * k
+				- p.nc * (omega / (k * p.betta_root_c) - p.bulk_to_term_c) * Z((omega - T(1.)) / (k * p.betta_root_c) - p.bulk_to_term_c)
+				- p.nh * (omega / (k * p.betta_root_h) - p.bulk_to_term_h) * Z((omega - T(1.)) / (k * p.betta_root_h) - p.bulk_to_term_h);
+		}
+
+		__device__ DispersionRelation(F_t Z, PhysicalParamenters<T> p): Z(Z), p(p) { }
+
+	private:
+		F_t Z;
+		PhysicalParamenters<T> p;
+	};
+
+	template <typename T, typename F_t>
+	struct DispersionRelationDerivative {
+		__device__ T operator()(T omega, T k) {
+			T arg_c = (omega - T(1.)) / (k * p.betta_root_c) - p.bulk_to_term_c;
+			T arg_h = (omega - T(1.)) / (k * p.betta_root_h) - p.bulk_to_term_h;
+			T Zc = Z(arg_c), Zh = Z(arg_h);
+			return p.nc / (k * p.betta_root_c) * (-Zc + (omega / (k * p.betta_root_c) - p.bulk_to_term_c) * (Zc * arg_c + T(1.)))
+				+ p.nh / (k * p.betta_root_h) * (-Zh + (omega / (k * p.betta_root_h) - p.bulk_to_term_h) * (Zh * arg_h + T(1.)));
+		}
+
+		__device__ DispersionRelationDerivative(F_t Z, PhysicalParamenters<T> p) : Z(Z), p(p) { }
+
+	private:
+		F_t Z;
+		PhysicalParamenters<T> p;
+	};
 
 } /* cuda */ } /* whfi */ } /* iki */
 
