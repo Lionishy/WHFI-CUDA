cd17a3e53431452d0e24686dabc832d71b80a6a9
Mark Shevelev <lionishy@gmail.com>
2019-09-16 11:51:13 +0300

    Device based code for dispersion relation updated

--- DispersionRelation.cuh
+++ DispersionRelation.cuh
@@ -23,7 +23,7 @@ namespace iki {	namespace whfi { namespace device {
 	};
 
 	template <typename T, typename ZFunc_t>
-	struct DispersionRelationDerivative {
+	struct DispersionRelationOmegaDerivative {
 		__device__ T operator()(T omega, T k) {
 			T arg_c = (omega - T(1.)) / (k * p.betta_root_c) - p.bulk_to_term_c;
 			T arg_h = (omega - T(1.)) / (k * p.betta_root_h) - p.bulk_to_term_h;
@@ -33,6 +33,26 @@ namespace iki {	namespace whfi { namespace device {
 		}
 
 		__device__ DispersionRelationDerivative(ZFunc_t Z, PhysicalParamenters<T> p) : Z(Z), p(p) { }
+
+	private:
+		ZFunc_t Z;
+		PhysicalParamenters<T> p;
+	};
+
+	template <typename T, typename ZFunc_t>
+	struct DispersionRelationKDerivative {
+		__device__ T operator()(T omega, T k) {
+			T arg_c = (omega - T(1.)) / (k * p.betta_root_c) - p.bulk_to_term_c;
+			T arg_h = (omega - T(1.)) / (k * p.betta_root_h) - p.bulk_to_term_h;
+			T Zc = Z(arg_c), Zh = Z(arg_h);
+			return T(2.) * k
+				+ p.nc * (omega / (k * k * p.betta_root_c) + p.bulk_to_term_c) * Zc
+				- p.nc * (omega / (k * p.betta_c) - p.bulk_to_term_c) * (Zc * arg_c + T(1.)) * (omega - T(1.)) / (k * k * p.betta_root_c)
+				+ p.nh * (omega / (k * k * p.betta_root_h) + p.bulk_to_term_h) * Zh
+				- p.nh * (omega / (k * p.betta_h) - p.bulk_to_term_h) * (Zh * arg_h + T(1.)) * (omega - T(1.)) / (k * k * p.betta_root_h);
+		}
+
+		__device__ DispersionRelationKDerivative(ZFunc_t Z, PhysicalParamenters<T> p) : Z(Z), p(p) { }
 
 	private:
 		ZFunc_t Z;
